b4b695e640736109572ae1ac03530ae8d9861a6b
ValeryMaz <mazhytova1996@gmail.com>
2025-07-04 18:50:59 -0400

    change directory

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 import css from './App.module.css';
-import CafeInfo from './components/CafeInfo/CafeInfo';
-import type { VoteType, VoteKey } from './types/votes';
-import VoteOptions from './components/VoteOptions/VoteOptions';
-import VoteStats from './components/VoteStats/VoteStats';
+import CafeInfo from '../CafeInfo/CafeInfo';
+import type { VoteType, VoteKey } from '../../types/votes';
+import VoteOptions from '../VoteOptions/VoteOptions';
+import VoteStats from '../VoteStats/VoteStats';
 
 function App() {
   const [votes, setVotes] = useState<VoteType>({
